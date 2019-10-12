f28f430bbd401c416c8b1319a134f600e8391a6b
msekibe <msekibe@ysk.co.jp>
2019-10-12 21:57:43 +0900

    Add DSL

---
+++ nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS Job') {
+    scm {
+        git('https://github.com/msekibe/devops_sample_nodejs.git') {  node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('msekibe@ysk.co.jp')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs_v11')
+    }
+    steps {
+        shell("npm install")
+        shell("npm test")
+    }
+}
